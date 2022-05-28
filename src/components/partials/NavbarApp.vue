adc02ffd7b77e3ebe33fce0afdfa39efb41d6bfa
masfuulaji <masfuulaji@gmail.com>
2022-05-28 21:05:27 +0700

    make ugly landing page part 2

--- src/components/partials/NavbarApp.vue
+++ src/components/partials/NavbarApp.vue
@@ -17,17 +17,32 @@
             </button>
         </div>
     </div>
-    <div v-if="menu_bar" class="fixed top-0 left-0 bg-red-600">
-        allo
+    <div v-if="menu_bar" class="fixed top-0 left-0 bg-ldblue-100 w-2/3 h-full border-r-2 border-teal-600">
+        <div class="w-full h-18 p-4 flex flex-wrap">
+            <div class="w-10 mr-auto">
+                <Logo />
+            </div>
+            <button type="button" @click="onMenu">
+                <font-awesome-icon class="mr-2 text-slate-300" icon="angles-left" size="xl" />
+            </button>
+        </div>
+        <div class="w-full h-full">
+            <SidebarLink text="Products" to="landing" icon="house"/>
+            <SidebarLink text="Features" to="landing" icon="bolt"/>
+            <SidebarLink text="Company" to="landing" icon="box"/>
+            <SidebarLink text="Application" to="landing" icon="brain"/>
+            <SidebarLink text="About" to="landing" icon="bugs"/>
+        </div>
     </div>
 </template>
 
 <script>
 import Logo from '@/assets/img/Logo.svg'
 import NavbarLink from '@/components/shared/NavbarLink.vue'
+import SidebarLink from '@/components/shared/SidebarLink.vue'
 import { ref } from 'vue'
 export default {
-    components: { Logo, NavbarLink },
+    components: { Logo, NavbarLink, SidebarLink },
     setup() {
         const menu_bar = ref(false)
         const onMenu = () => {
---
+++ src/components/shared/SidebarLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link :to="{ name: to }">
+        <div class="rounded-lg bg-ldblue-100 hover:bg-indigo-600 mx-5 p-2">
+            <div class="mx-2 my-1 font-semibold text-sm lg:text-2xl text-slate-200 tracking-wider">
+                <font-awesome-icon class="mr-2 text-slate-300" :icon="icon" size="sm" />{{ text }}
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: ['text', 'to', 'icon'],
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
